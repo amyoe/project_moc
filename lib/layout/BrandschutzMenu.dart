5a15f565feea2639ff84dd99402704c558f1deef
MalteH96 <94699226+MalteH96@users.noreply.github.com>
2021-12-10 11:06:58 +0100

    ComplianceMenü

---
+++ lib/layout/BrandschutzMenu.dart
@@ -0,0 +1,17 @@
+import 'package:project_moc/layout/widget.dart';
+import 'package:flutter/material.dart';
+import 'package:project_moc/layout/game';
+
+class BrandschutzMenu extends StatefulWidget {
+  const BrandschutzMenu({Key? key}) : super(key: key);
+
+  @override
+  _BrandschutzMenuState createState() => _BrandschutzMenuState();
+}
+
+class _BrandschutzMenuState extends State<BrandschutzMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/layout/Compliance Schulungen.dart
+++ lib/layout/Compliance Schulungen.dart
@@ -4,8 +4,22 @@ import 'package:flutter/material.dart';
 import 'package:project_moc/layout/games.dart';
 
 
-class ComplianceOverview extends StatelessWidget {
+class ComplianceOverview extends StatefulWidget {
   const ComplianceOverview({Key? key}) : super (key: key);
+
+  @override
+  State<ComplianceOverview> createState() => _ComplianceOverviewState();
+}
+
+class _ComplianceOverviewState extends State<ComplianceOverview> {
+
+  int brandschutzLevel = 0;
+  int sozialLevel = 0;
+  int gesundheitsLevel = 0;
+  int maxBrandschutzLevel = 0;
+  int maxSozialLevel = 0;
+  int maxGesundheitsLevel = 0;
+
 
   @override
     Widget build(BuildContext context) {
@@ -23,7 +37,10 @@ class ComplianceOverview extends StatelessWidget {
                         builder: (context) => const GamesChoice()),
                   );
                 },
-                child: const Text("Brandschutz"),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: const Text("Brandschutz"),
+                )
             ),
             ElevatedButton(
               onPressed: () {
@@ -33,7 +50,10 @@ class ComplianceOverview extends StatelessWidget {
                       builder: (context) => const GamesChoice()),
                 );
               },
-              child: const Text("Arbeitssicherheit"),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: const Text("Gesundheitsschutz"),
+                ),
             ),
             ElevatedButton(
               onPressed: () {
@@ -43,17 +63,10 @@ class ComplianceOverview extends StatelessWidget {
                       builder: (context) => const GamesChoice()),
                 );
               },
-              child: const Text("Gesundheitsschutz"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const GamesChoice()),
-                );
-              },
-              child: const Text("Service- und Sozialangebote"),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: const Text("Service- und Sozialangebote"),
+              ),
             ),
           ],
     ),
